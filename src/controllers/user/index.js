80ad901e9f95a5ad1c1fbe8f212200857a2dc4cf
keyukidau4 <keyukidau4@gmail.com>
2024-08-17 16:18:16 +0900

    updat

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -1,36 +1,36 @@
-import { createUser } from "../../models/user/index.js";
-import { getUserByEmail } from "../../models/user/index.js";
+// import { createUser } from "../../models/user/index.js";
+// import { getUserByEmail } from "../../models/user/index.js";
 
-export const register = async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const name = req.body.name;
+// export const register = async (req, res) => {
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   const name = req.body.name;
 
-  if (!email || !password || !name) {
-    return res.status(400).send({
-      code: 400,
-      message: "Email or Password is required",
-    });
-  }
+//   if (!email || !password || !name) {
+//     return res.status(400).send({
+//       code: 400,
+//       message: "Email or Password is required",
+//     });
+//   }
 
-  const checkUser = await getUserByEmail(email);
+//   const checkUser = await getUserByEmail(email);
 
-  if (checkUser) {
-    return res.status(400).send({
-      code: 400,
-      message: "Account is Existed!",
-    });
-  }
+//   if (checkUser) {
+//     return res.status(400).send({
+//       code: 400,
+//       message: "Account is Existed!",
+//     });
+//   }
 
-  const users = {
-    name,
-    email,
-    password,
-  };
+//   const users = {
+//     name,
+//     email,
+//     password,
+//   };
 
-  const newUser = await createUser(users);
+//   const newUser = await createUser(users);
 
-  // loggers.info({ newUser });
+//   // loggers.info({ newUser });
 
-  return;
-};
+//   return;
+// };
