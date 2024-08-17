e622d0d7d36937bc6b8daa0a8356ef342491d69e
keyukidau4 <keyukidau4@gmail.com>
2024-08-17 16:09:11 +0900

    up

--- src/models/user/index.js
+++ src/models/user/index.js
@@ -1,55 +1,55 @@
-import mongoose, { mongo } from "mongoose";
+// import mongoose, { mongo } from "mongoose";
 
-const UserSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      select: false,
-    },
-    salt: {
-      type: String,
-      select: false,
-    },
-    watchedMovieId: {
-      type: Array,
-      default: [],
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+// const UserSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//       select: false,
+//     },
+//     salt: {
+//       type: String,
+//       select: false,
+//     },
+//     watchedMovieId: {
+//       type: Array,
+//       default: [],
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
 
-export const UserModel = mongoose.model("users", UserSchema);
+// export const UserModel = mongoose.model("users", UserSchema);
 
-export const createUser = (users) => UserModel.create(users);
+// export const createUser = (users) => UserModel.create(users);
 
-export const findUserById = (id) => UserModel.findById(id);
+// export const findUserById = (id) => UserModel.findById(id);
 
-export const deleteUserById = (id) => UserModel.deleteUserById(id);
+// export const deleteUserById = (id) => UserModel.deleteUserById(id);
 
-export const getUserByEmail = (email) => UserModel.find({ email });
+// export const getUserByEmail = (email) => UserModel.find({ email });
 
-export const addWatchedMovie = async (id, movieId) => {
-  const user = await UserModel.findById(id);
+// export const addWatchedMovie = async (id, movieId) => {
+//   const user = await UserModel.findById(id);
 
-  const movieList = user.watchedMovieId;
+//   const movieList = user.watchedMovieId;
 
-  if (movieList.includes(movieId)) {
-    return movieList;
-  }
+//   if (movieList.includes(movieId)) {
+//     return movieList;
+//   }
 
-  movieList.push(movieId);
+//   movieList.push(movieId);
 
-  return movieList;
-};
+//   return movieList;
+// };
